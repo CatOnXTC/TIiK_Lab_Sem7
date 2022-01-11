1c92769a2c758200709c9a19e8ce0d074ea380c7
CatOnXTC <karolsienkiewicz03@gmail.com>
2022-01-11 20:24:54 +0100

    final final

--- lzss.py
+++ lzss.py
@@ -1,44 +1,92 @@
 import codecs
+from ctypes import BigEndianStructure
 import re
-import sys
+import encodings
+
+def add_to_dictionary(dictionary, to_add):
+    if(len(dictionary + to_add)>4095):
+        dictionary += to_add
+        dictionary = dictionary[4095:]
+    else:
+        dictionary += to_add
+    return dictionary
+
 def compress(filename):
     f = codecs.open(filename,"r","utf-8").read()
-    buffer= ""
-    output=""
+    file = open("compressed.txt", "wb")
+
+    buffer=""
+    dictionary = ""
     i=0
     while(i<len(f)):
-        character= f[i]
-        if buffer+character in output:
+        character = f[i]
+        if buffer+character in dictionary and len(buffer) < 15:
             buffer+=character
             i+=1
+        elif len(buffer) == 0:
+            file.write(character.encode('utf-8'))
+            dictionary = add_to_dictionary(dictionary,character)
+            i+=1
         else:
-            if len(buffer)> (len(str(len(buffer)))+len(str(output.find(buffer)))+3):
-                output += "("+output.find(buffer)+","+ len(buffer)+")"
+            if len(buffer.encode('utf-8')) >= 3:
+                file.write('\7'.encode('utf-8'))
+                temp = dictionary.find(buffer)
+                temp = temp << 4
+                temp = temp | len(buffer)
+                file.write(temp.to_bytes(2,byteorder ='big'))
             else:
-                output += buffer+character
-                i+=1
-            buffer=""
-    print("Done")
-    file = codecs.open("compressed.txt", "w","utf-8")
-    file.write(output)
+                file.write(buffer.encode('utf-8'))
+            dictionary = add_to_dictionary(dictionary,buffer)
+            buffer = ""
+    #Wypisz resztki pominięte przez pętlę
+    if len(buffer) != 0:
+        if buffer in dictionary:
+            if len(buffer.encode('utf-8')) >= 3:
+                file.write('\7'.encode('utf-8'))
+                temp = dictionary.find(buffer)
+                temp = temp << 4
+                temp = temp | len(buffer)
+                file.write(temp.to_bytes(2,byteorder ='big'))
+            else:
+                file.write(buffer.encode('utf-8'))
+            dictionary = add_to_dictionary(dictionary,buffer)
+            buffer = ""
+        else:
+            file.write(character.encode('utf-8'))
+            dictionary = add_to_dictionary(dictionary,character)
+
     file.close()
+    f.close()
+
 
 def decompress(filename):
-    f = codecs.open(filename,"r","utf-8").read()
-    output=f
-    regex = re.findall("(\([0-9]+,[0-9]+\))", output)  
-    for sub in regex:
-        temp = sub.replace("(","").replace(")","")
-        index = int(temp.split(",")[0])
-        length = int(temp.split(",")[1])
-        output=output.replace(sub,f[index:(index+length)])
-    file = codecs.open("decompressed.txt", "w","utf-8")
-    file.write(output)
-    file.close() 
+    f = open(filename,"rb")
+    file = open("decompressed.txt", "wb")
+    dictionary = ""
+    buffer = b''
+    character = f.read(1)
+    while(character):
+        if(character != '\7'.encode('utf-8')):
+            file.write(character)
+            buffer += character
+            character = f.read(1)
+        else:
+            temp_dict = buffer.decode('utf-8')
+            dictionary = add_to_dictionary(dictionary,temp_dict)
+            buffer = b''
+            #Po zaktualizowania słownika można wyszukać w nim odpowiednią zawartość
+            character = f.read(2)
+            character = int.from_bytes(character, byteorder='big')
+            temp1 = character >> 4
+            temp2 = character & 15
+            found = dictionary[temp1:temp1+temp2]
+            file.write(found.encode('utf-8'))
+            dictionary = add_to_dictionary(dictionary,found)
+            character = f.read(1)
+
+    file.close()
+    f.close() 
 
 
-if __name__ == "__main__":
-    filename = (sys.argv[1])
-    compress(filename)
-    decompress("compressed.txt")
-
+#compress("literacki_pl.txt")
+decompress("compressed.txt")
